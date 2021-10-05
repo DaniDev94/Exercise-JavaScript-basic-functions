738d6864ae32e8085506dfb23e30d034c48cd54e
Daniel López Plaza <daniel.lp1094@gmail.com>
2021-10-05 18:33:27 +0200

    FEAT: iteration#4

---
+++ iteration-4/iteration#4.js
@@ -0,0 +1,12 @@
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+
+function average(total) {
+  let sum = 0;
+  for (let num of total) {
+    sum += num;
+  }
+  let div = sum / total.length;
+  return div;
+}
+
+console.log(average(numbers));
