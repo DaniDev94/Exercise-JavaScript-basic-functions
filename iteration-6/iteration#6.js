70a2cff45faa38e41f3fb8a46402400044010932
Daniel López Plaza <daniel.lp1094@gmail.com>
2021-10-05 23:22:00 +0200

    FEAT: iteration#6

---
+++ iteration-6/iteration#6.js
@@ -0,0 +1,21 @@
+const duplicates = [               
+  "sushi",//no
+  "pizza",//si
+  "burger",//no
+  "potatoe",//no
+  "pasta",//si
+  "ice-cream",//no
+  "pizza",//si
+  "chicken",//no
+  "onion rings",//no
+  "pasta",//si
+  "soda",//no
+];
+/* const finalResult = ["sushi", "pizza", "burger", "potatoe", "pasta","ice-cream", "chicken","onion rings", "soda"]; */
+
+function removeDuplicates(elemets) {
+    var noRemove = [...new Set(elemets)]
+    console.log(noRemove);
+};
+
+removeDuplicates(duplicates);
