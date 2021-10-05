0d718964fa1c365e21f1db6e3d7338c0a9af5f5f
Daniel López Plaza <daniel.lp1094@gmail.com>
2021-10-05 17:38:30 +0200

    FEAT: iteration#3

---
+++ iteration-3/iteration#3.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+let sum = 0;
+function sumAll(total) {
+    for (let num of total){
+        sum +=  num
+    };
+    console.log(sum);
+};
+sumAll(numbers);
