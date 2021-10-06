087754eed1d5c5a9f58b45008a86a7fe6cd89001
Daniel López Plaza <daniel.lp1094@gmail.com>
2021-10-06 17:05:04 +0200

    FEAT: iteration#6

--- iteration-6/iteration#6.js
+++ iteration-6/iteration#6.js
@@ -1,24 +1,34 @@
-const duplicates = [               
-  "sushi",//no
-  "pizza",//si
-  "burger",//no
-  "potatoe",//no
-  "pasta",//si
-  "ice-cream",//no
-  "pizza",//si
-  "chicken",//no
-  "onion rings",//no
-  "pasta",//si
-  "soda",//no
+const duplicates = [
+  "sushi", //no
+  "pizza", //si
+  "burger", //no
+  "potatoe", //no
+  "pasta", //si
+  "ice-cream", //no
+  "pizza", //si
+  "chicken", //no
+  "onion rings", //no
+  "pasta", //si
+  "soda", //no
 ];
 /* const finalResult = ["sushi", "pizza", "burger", "potatoe", "pasta","ice-cream", "chicken","onion rings", "soda"]; */
 
 function removeDuplicates(elemets) {
-    var noRemove = [...new Set(elemets)]
-    console.log(noRemove);
-};
+  var noRemove = [...new Set(elemets)];
+  console.log(noRemove);
+}
 
 removeDuplicates(duplicates);
 
 
+console.log("Prueba 2______________________________________________________________________________________________");
 
+
+function repeatCounter2(elements) {
+  let countElements = {};
+  for (let i = 0; i < elements.length; i++) {
+    countElements[elements[i]]++;
+  }
+  console.log(Object.keys(countElements))
+}
+repeatCounter2(duplicates);
