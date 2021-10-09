c280730315fd47f77a5542b3542e3ec4808db5f3
Daniel Lopez Plaza <daniel.lp1094@gmail.com>
2021-10-09 18:10:46 +0000

    "FEAT: iteration#2."

--- iteration-2/iteration#2.js
+++ iteration-2/iteration#2.js
@@ -2,8 +2,8 @@ console.log("Prueba 1___________________________________________________________
 
 const avengers = ["Hulk", "Thor", "IronMan", "Captain A.", "Spiderman", "Captain M."];
 // console.log('avengers-->',avengers[0].length); = 4 
-let total = avengers[0]
 function findLongestWord(heroes) {
+let total = heroes[0]
     for(let hero of heroes){
         // console.log('hero-->',hero.length) = 4, 4, 7, 10, 9, 10
         if(hero.length > total.length){
